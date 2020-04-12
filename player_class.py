7d0b2ae83c496035617ac0cd263aacbeea85e2ae
liliancz <46685381+liliancz@users.noreply.github.com>
2020-04-12 11:01:20 -0500

    updated player movement

--- player_class.py
+++ player_class.py
@@ -8,11 +8,20 @@ class Player:
         self.grid_pos = pos
         self.pix_pos = self.get_pix_pos()
         self.direction = vec(1,0)
+        self.stored_direction = None
         
         
     def update(self):
         self.pix_pos += self.direction
-
+        if int(self.pix_pos.x+TOP_BOTTOM_BUFFER//2) % self.app.cell_width == 0:
+            if self.direction == vec(1,0) or self.direction == vec (-1,0):
+                if self.stored_direction != None:
+                    self.direction = self.stored_direction
+        if int(self.pix_pos.y+TOP_BOTTOM_BUFFER//2) % self.app.cell_height == 0:
+            if self.direction == vec(0,1) or self.direction == vec (0,-1):
+                if self.stored_direction != None:
+                    self.direction = self.stored_direction
+        
         # settings grid position in reference to pixel pos
         self.grid_pos[0] = (self.pix_pos[0]-TOP_BOTTOM_BUFFER+self.app.cell_width//2)//self.app.cell_width+1
         self.grid_pos[1] = (self.pix_pos[1]-TOP_BOTTOM_BUFFER+self.app.cell_height//2)//self.app.cell_height+1
@@ -29,7 +38,7 @@ class Player:
                          self.app.cell_width, self.app.cell_height), 1)
 
     def move(self, direction):
-        self.direction = direction
+        self.stored_direction = direction
 
     def get_pix_pos(self):
         return  vec((self.grid_pos.x*self.app.cell_width)
